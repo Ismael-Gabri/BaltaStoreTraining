b8c5318d4c94bcd8c8995a9a9e1e5557fa25865d
Ismael Castro <Ismaelgabrijob@hotmail.com>
2022-10-01 19:39:36 -0300

    Adicionar arquivos de projeto.

---
+++ BaltaStore.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace BaltaStore.Domain;
+public class Class1
+{
+
+}
---
+++ BaltaStore.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BaltaStore.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
